d782d0a88de7f881f6902cb2a37d0231eb8b9d66
AsamaKiseto <974616825@qq.com>
2022-08-24 07:01:09 +0000

    nse

--- nse/2_nse_operator_amlt.py
+++ nse/2_nse_operator_amlt.py
@@ -15,20 +15,20 @@ def get_args(argv=None):
     parser.add_argument('-w', '--width', default=32, type=int, help='the number of width of FNO layer')
     
     parser.add_argument('--batch_size', default=64, type=int, help = 'batch size')
-    parser.add_argument('--epochs', default=1000, type=int, help = 'Number of Epochs')
+    parser.add_argument('--epochs', default=500, type=int, help = 'Number of Epochs')
     parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
     parser.add_argument('--wd', default=1e-4, type=float, help='weight decay')
-    parser.add_argument('--step_size', default=200, type=int, help='scheduler step size')
+    parser.add_argument('--step_size', default=100, type=int, help='scheduler step size')
     parser.add_argument('--gamma', default=0.5, type=float, help='scheduler factor')
     parser.add_argument('--gpu', default=0, type=int, help='device number')
 
-    parser.add_argument('-tg', '--tg', default=20, type=int, help = 'time gap')
+    parser.add_argument('-tg', '--tg', default=1, type=int, help = 'time gap')
     parser.add_argument('-Ng', '--Ng', default=1, type=int, help = 'N gap')
     parser.add_argument('-l1', '--lambda1', default=1, type=float, help='weight of losses1')
     parser.add_argument('-l2', '--lambda2', default=0.1, type=float, help='weight of losses2')
     parser.add_argument('-l3', '--lambda3', default=0.01, type=float, help='weight of losses3')
     parser.add_argument('-l4', '--lambda4', default=0.1, type=float, help='weight of losses4')
-    parser.add_argument('-fc', '--f_channels', default=4, type=int, help='channels of f encode')
+    parser.add_argument('-fc', '--f_channels', default=1, type=int, help='channels of f encode')
     
     return parser.parse_args(argv)
 
@@ -58,7 +58,6 @@ if __name__=='__main__':
     # model setting
     nse_model = NSEModel(args, shape, data)
     params_num = nse_model.cout_params()
-
     nse_model.print_params()
 
     print('N0: {}, nt: {}, nx: {}, ny: {}, device: {}'.format(N0, nt, nx, ny, nse_model.device))
@@ -68,7 +67,6 @@ if __name__=='__main__':
     print(f'obs: {logs["data_norm"]["obs"]}')
     print(f'param numbers of the model: {params_num}')
 
-    
     nse_model.process()
     logs['logs'] = nse_model.get_logs()
     torch.save([nse_model.model.state_dict(), logs], logs_fname)
--- nse/test2.py
+++ nse/test2.py
@@ -6,95 +6,91 @@ import matplotlib.pyplot as plt
 from  matplotlib import colors
 import numpy as np
 
-cdict = {'red':   ((0.0,  0.22, 0.0),
-                   (0.5,  1.0, 1.0),
-                   (1.0,  0.89, 1.0)),
+from scripts.utils import *
 
-         'green': ((0.0,  0.49, 0.0),
-                   (0.5,  1.0, 1.0),
-                   (1.0,  0.12, 1.0)),
-
-         'blue':  ((0.0,  0.72, 0.0),
-                   (0.5,  0.0, 0.0),
-                   (1.0,  0.11, 1.0))}
-
-cmap = colors.LinearSegmentedColormap('custom', cdict)
-
-def f(x):
-    return x
+def f(x, y):
+    return torch.exp(x + y)
+    # return x + y
 
 class test1(nn.Module):
     def __init__(self):
         super(test1, self).__init__()
-        self.width1 = 4
-        self.width2 = 8
-        self.fc0 = nn.Linear(1, self.width1)
-        self.fc1 = nn.Linear(self.width1, self.width2)
-        self.fc2 = nn.Linear(self.width1+self.width2 , self.width1)
-        self.fc3 = nn.Linear(self.width1, 1)
+        self.fc0 = nn.Linear(2, 8)
+        self.fc1 = nn.Linear(8, 32)
+        self.fc2 = nn.Linear(40, 16)
+        self.fc3 = nn.Linear(16, 4)
+        self.fc4 = nn.Linear(4, 1)
     
     def forward(self, x):
-        x = x.reshape(x.shape[0], 1)
+        x = x.reshape(x.shape[0], -1)
         x = self.fc0(x)
-        # x = F.gelu(x)
+        x = F.gelu(x)
         xf = self.fc1(x)
-        # xf = F.gelu(xf)
+        xf = F.gelu(xf)
         # print(xf.shape, x.shape)
-        xf = xf.mean(0).reshape(1, self.width2).repeat(x.shape[0], 1)
+        xf = xf.mean(0).reshape(1, 32).repeat(x.shape[0], 1)
         x = torch.cat((x, xf), dim=-1)
         x = self.fc2(x)
-        # x = F.gelu(x)
+        x = F.gelu(x)
         x = self.fc3(x)
+        x = F.gelu(x)
+        x = self.fc4(x)
         
         return x
 
 class test2(nn.Module):
     def __init__(self):
         super(test2, self).__init__()
-        self.width1 = 4
-        self.width2 = 8
-        self.fc0 = nn.Linear(1, self.width1)
-        self.fc1 = nn.Linear(self.width1, self.width2)
-        self.fc2 = nn.Linear(self.width2, self.width1)
-        self.fc3 = nn.Linear(self.width1, 1)
+        self.fc0 = nn.Linear(2, 8)
+        self.fc1 = nn.Linear(8, 32)
+        self.fc2 = nn.Linear(32, 16)
+        self.fc3 = nn.Linear(16, 4)
+        self.fc4 = nn.Linear(4, 1)
     
     def forward(self, x):
-        x = x.reshape(x.shape[0], 1)
+        x = x.reshape(x.shape[0], -1)
         x = self.fc0(x)
-        # x = F.gelu(x)
+        x = F.gelu(x)
         x = self.fc1(x)
-        # x = F.gelu(x)
+        x = F.gelu(x)
         # print(xf.shape, x.shape)
         x = self.fc2(x)
-        # x = F.gelu(x)
+        x = F.gelu(x)
         x = self.fc3(x)
+        x = F.gelu(x)
+        x = self.fc4(x)
         
         return x
 
 class testData(Dataset):
-    def __init__(self, x, y):
-        self.ipt = x
-        self.opt = y
+    def __init__(self, ipt, opt):
+        self.ipt = ipt
+        self.opt = opt
 
     def __len__(self):
-        return len(x)
+        return self.ipt.shape[0]
     
     def __getitem__(self, idx):
         x = torch.FloatTensor(self.ipt[idx])
         y = torch.FloatTensor(self.opt[idx])
         return x, y
 
-N = 1000
+N = 100
 epochs = 1000
+batchsize = 1000
 num = 10
 
 x = torch.rand(N)
 x = torch.arange(N) / N
+y = x
+x, y = torch.meshgrid(x, y)
 x = x.unsqueeze(-1)
-y = f(x)
+y = y.unsqueeze(-1)
+ipt = torch.cat((x, y), dim=-1).reshape(-1, 2)
+opt = f(x, y).reshape(-1, 1)
 
-data = testData(x, y)
-train_loader = DataLoader(dataset=data, batch_size=N, shuffle=True)
+data = testData(ipt, opt)
+train_loader = DataLoader(dataset=data, batch_size=batchsize, shuffle=True)
 
 model1 = test1()
 model2 = test2()
@@ -108,13 +104,13 @@ for epoch in range(1, epochs + 1):
         
         optimizer1.zero_grad()
         ym = model1(x)
-        loss = ((y - ym)**2).mean()
+        loss = ((y - ym)**2).sum()
 
         loss.backward()
         optimizer1.step()
 
-        if(epoch% (epochs//10)==0): 
-            print(f'# {epoch} loss: {loss}')
+    if(epoch% (epochs//10)==0): 
+        print(f'# {epoch} loss: {loss}')
 
 model1.eval()
 
@@ -125,25 +121,51 @@ for epoch in range(1, epochs + 1):
         
         optimizer2.zero_grad()
         ym = model2(x)
-        loss = ((y - ym)**2).mean()
+        loss = ((y - ym)**2).sum()
 
         loss.backward()
         optimizer2.step()
 
-        if(epoch%100==0): 
-            print(f'# {epoch} loss: {loss}')
+    if(epoch%100==0): 
+        print(f'# {epoch} loss: {loss}')    
 
 model2.eval()
 
 # xt1 = torch.tensor([0.5]).requires_grad_(True)
 # xt2 = torch.tensor([0.5]).requires_grad_(True)
-xt1 = torch.rand(num).requires_grad_(True)
-xt2 = torch.rand(num).requires_grad_(True)
-yt1 = f(xt1)
-yt2 = f(xt2)
-xt = x.requires_grad_(True)
-ym1 = model1(xt1).squeeze()
-ym2 = model2(xt2).squeeze()
+num = 100
+
+xt = (torch.arange(num) / num)
+yt = (torch.arange(num) / num)
+xt = torch.rand(num)
+yt = torch.rand(num)
+xt, yt = torch.meshgrid(xt, yt)
+xt = xt.unsqueeze(-1)
+yt = yt.unsqueeze(-1)
+ipt = torch.cat((xt, yt), dim=-1).reshape(-1, 2)
+zt = f(ipt[:, 0], ipt[:, 1]).squeeze()
+ipt1 = ipt.requires_grad_(True)
+ipt2 = ipt.requires_grad_(True)
+zm1 = model1(ipt1).squeeze()
+zm2 = model2(ipt2).squeeze()
+var1 = abs_error(zm1, zt)
+var2 = abs_error(zm2, zt)
+print(var1, var2)
+zm1[0].backward()
+ipt1.grad
+
+
+xt1 = torch.rand(num).requires_grad_(True).unsqueeze(-1)
+xt2 = torch.rand(num).requires_grad_(True).unsqueeze(-1)
+yt1 = torch.rand(num).requires_grad_(True).unsqueeze(-1)
+yt2 = torch.rand(num).requires_grad_(True).unsqueeze(-1)
+ipt1 = torch.cat((xt1, yt1), dim=-1)
+ipt2 = torch.cat((xt2, yt2), dim=-1)
+zt1 = f(xt1, yt1).squeeze()
+zt2 = f(xt2, yt2).squeeze()
+
+zm1 = model1(ipt1).squeeze()
+zm2 = model2(ipt2).squeeze()
 # ym = model1(xt)
 
 k = 0
