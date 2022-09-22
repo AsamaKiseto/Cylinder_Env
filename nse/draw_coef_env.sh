dd16f105c873a47ca617d03ebeaf8ed802bbb6cb
AsamaKiseto <974616825@qq.com>
2022-09-22 14:34:34 +0000

    nse

--- nse/draw_coef_env.sh
+++ nse/draw_coef_env.sh
@@ -1,7 +1,7 @@
-for scale in 0 0.5 1.0; do
+for scale in 0.1 0.5 1.0; do
     python3 4_draw_coef_env.py -s $scale -nt 80 -ts 5 -f 0.14285714285714285 -lf 1
 done
 
-for scale in 0 0.5 1.0; do
+for scale in 0.1 0.5 1.0; do
     python3 4_draw_coef_env.py -s $scale -nt 80 -ts 5 -f 1 -lf 2
 done
--- nse/draw_coef_test.sh
+++ nse/draw_coef_test.sh
@@ -1,3 +1,7 @@
+
+python3 4_draw_coef_env.py -s 0.0 -nt 80 -ts 5 -f 0.14285714285714285 -lf 1
+python3 4_draw_coef_env.py -s 0.0 -nt 80 -ts 5 -f 1 -lf 2
+
 for k in {1..10}; do
     bash draw_coef_env.sh
 done
