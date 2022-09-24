71ac48bae81f205be101a76c04533581dd0e1a17
AsamaKiseto <974616825@qq.com>
2022-09-24 14:33:10 +0000

    nse

--- nse/1_data_gen_test.py
+++ nse/1_data_gen_test.py
@@ -13,15 +13,15 @@ import argparse
 
 def get_args(argv=None):
     parser = argparse.ArgumentParser(description='Put your hyperparameters')
-    
-    parser.add_argument('--f1', default=-3, type=float)
-    parser.add_argument('--f2', default=-3, type=float)
+    parser.add_argument('-dn', '--data_name', default="test", type=str)
+    parser.add_argument('-fb', '--f_base', default=-1, type=float)
+    parser.add_argument('-s', '--scale', default=0, type=float)
 
     return parser.parse_args(argv)
 
 # env init
 env = Cylinder_Rotation_Env(params={'dtr': 0.01, 'T': 1, 'rho_0': 1, 'mu' : 1/1000,
-                                    'traj_max_T': 20, 'dimx': 128, 'dimy': 64,
+                                    'traj_max_T': 20, 'dimx': 256, 'dimy': 64,
                                     'min_x' : 0,  'max_x' : 2.2, 
                                     'min_y' : 0,  'max_y' : 0.41, 
                                     'r' : 0.05,  'center':(0.2, 0.2),
@@ -38,56 +38,70 @@ if __name__ == '__main__':
 
     # param setting
     dt = env.params['dtr'] * env.params['T']
-    nT_init = 300
-    nT = nT_init + 100
+    nT = 400
+    hf_nT = int(nT/2)
     nx = env.params['dimx']
     ny = env.params['dimy']
     print(f'dt: {dt} | nt: {nT}')
 
     # data generate
-    N0 = 10
-    t = np.arange(nT)
-    f = np.linspace(-5, 5, N0)
-    print(f'f: {f}')
-    
+    N0 = args.N0
+    tg = 5
+    nt = int(nT / tg)
+
     obs = np.zeros((N0, nT+1, nx, ny, 5))
     print(f'state_data_size :{obs.shape}')
-    Cd, Cl = np.zeros((N0, nT)), np.zeros((N0, nT))
+    ctr = np.zeros((N0, nT))
+    C_D, C_L = np.zeros((N0, nT)), np.zeros((N0, nT))
 
-    print('start')
-    obs_init = np.zeros((nT_init+1, nx, ny, 5))
-    Cd_init, Cl_init = np.zeros(nT_init), np.zeros(nT_init)
-    obs_init[0] = env.reset()
-
+    # env init step
     start = default_timer()
+    nT_init = 10
     for i in range(nT_init):
-        obs_init[i+1], Cd_init[i], Cl_init[i] = env.step(0.00)
+        env.step(0.00)
     end = default_timer()
     print(f'init complete: {end - start}')
 
     env.set_init()
-    
+    # obs_temp = env.reset(mode='vertex')
+    # obs_temp = env.reset(mode='grid')
+    # mesh_num = obs_temp.shape[0]
+    # obs_v = np.zeros((N0, nT+1, mesh_num, 5))
+
     for k in range(N0):
-        print(f'# {k}')
+        ctr_temp = args.scale(np.random.rand(nt) - 0.5) + args.f_base
+        ctr_temp = ctr_temp.reshape(nt, 1).repeat(tg, 1).reshape(-1)
+        ctr[k] = ctr_temp
+
+        print(f'start # {k}')
         start = default_timer()
-        
-        obs[k, :nT_init+1], Cd[k, :nT_init], Cl[k, :nT_init] = obs_init, Cd_init, Cl_init
-        env.reset()
 
-        for i in range(nT_init, nT):
-            obs[k, i+1], Cd[k, i], Cl[k, i] = env.step(f[k])
-
+        # obs_v[Nf * k + l, 0] = env.reset(mode='vertex')
+        obs[k, 0] = env.reset(mode='grid')
+    
+        for i in range(nT):
+            obs[k, i+1], C_D[k, i], C_L[k, i] = env.step(ctr_temp[i])
+            # obs_v[Nf * k + l, i+1], C_D[Nf * k + l, i], C_L[Nf * k + l, i] = env.step(f1[k], mode='vertex')
+    
         end = default_timer()
-        print(f'end | time: {end - start}')
+
+        print(f'end # {k} | time: {end-start}')
+        # print(f'ang_vel: {ang_v}')
+        # print(f'reward :{reward[k]}')
 
     # np to tensor
     obs_tensor = torch.Tensor(obs)
-    Cd_tensor = torch.Tensor(Cd)
-    Cl_tensor = torch.Tensor(Cl)
-    f_tensor = torch.Tensor(f)
+    # obs_v_tensor = torch.Tensor(obs_v)
+    C_D_tensor = torch.Tensor(C_D)
+    C_L_tensor = torch.Tensor(C_L)
+    ctr_tensor = torch.Tensor(ctr)
 
-    data = [obs_tensor, Cd_tensor, Cl_tensor, f_tensor]
+    data = [obs_tensor, C_D_tensor, C_L_tensor, ctr_tensor]
+    # data_v = [obs_v_tensor, C_D_tensor, C_L_tensor, f_tensor]
 
     # save data
-    torch.save(data, 'data/nse_data_test')
-    
+    # torch.save(data, './data/nse_data_N0_{}_nT_{}_f1_{}_f2_{}'.format(N0, nT, args.f1, args.f2))
+    # torch.save(data_v, './data/nse_data_irr')
+    # torch.save(data, './data/nse_data_reg_extra')
+    torch.save(data, f'./data/test_data/nse_data_reg_scale_{args.scale}_{args.data_name}')
+    # torch.save(data, './data/nse_data_test1')
