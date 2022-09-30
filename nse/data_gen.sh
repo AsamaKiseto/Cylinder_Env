720eb8ad564cc5970eea68d9db5aa5cb96af62c7
AsamaKiseto <974616825@qq.com>
2022-09-30 03:47:45 -0700

    nse

--- nse/data_gen.sh
+++ nse/data_gen.sh
@@ -1,6 +1,8 @@
-python3 1_data_gen_test.py -fb 0.142857 -s 0.1 -dn 1_1
-python3 1_data_gen_test.py -fb 0.142857 -s 0.5 -dn 1_3
-python3 1_data_gen_test.py -fb 0.142857 -s 1.0 -dn 1_5
-python3 1_data_gen_test.py -fb 1.0 -s 0.1 -dn 2_1
-python3 1_data_gen_test.py -fb 1.0 -s 0.5 -dn 2_3
-python3 1_data_gen_test.py -fb 1.0 -s 1.0 -dn 2_5
+python3 1_data_gen_test.py -fb 0.142857 -s 0.1 -dn 1
+python3 1_data_gen_test.py -fb 0.142857 -s 0.5 -dn 1
+python3 1_data_gen_test.py -fb 0.142857 -s 0.8 -dn 1
+python3 1_data_gen_test.py -fb 0.142857 -s 1.0 -dn 1
+python3 1_data_gen_test.py -fb 1.0 -s 0.1 -dn 2
+python3 1_data_gen_test.py -fb 1.0 -s 0.5 -dn 2
+python3 1_data_gen_test.py -fb 1.0 -s 0.8 -dn 2
+python3 1_data_gen_test.py -fb 1.0 -s 1.0 -dn 2
