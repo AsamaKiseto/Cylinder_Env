96c74a840348ad644fa2ce6d8833e201c4f4cf05
AsamaKiseto <974616825@qq.com>
2022-10-01 21:42:00 -0700

    nse

--- nse/2_nse_operator_grid_test1.py
+++ nse/2_nse_operator_grid_test1.py
@@ -7,48 +7,46 @@ from scripts.nse_model import *
 def get_args(argv=None):
     parser = argparse.ArgumentParser(description = 'Put your hyperparameters')
 
-    parser.add_argument('-dp', '--data_path', default='nse_data_reg_extra', type=str, help='data path name')
+    parser.add_argument('-dp', '--data_path', default='nse_data_reg', type=str, help='data path name')
     parser.add_argument('-lf', '--logs_fname', default='test', type=str, help='logs file name')
     
-    parser.add_argument('--phys_gap', default=20, type=int, help = 'Number of gap of Phys')
-    parser.add_argument('--phys_epochs', default=10, type=int, help = 'Number of Phys Epochs')
+    parser.add_argument('-L', '--L', default=2, type=int, help='the number of layers')
+    parser.add_argument('-m', '--modes', default=16, type=int, help='the number of modes of Fourier layer')
+    parser.add_argument('-w', '--width', default=32, type=int, help='the number of width of FNO layer')
+    
+    parser.add_argument('--phys_gap', default=2, type=int, help = 'Number of gap of Phys')
+    parser.add_argument('--phys_epochs', default=1, type=int, help = 'Number of Phys Epochs')
     parser.add_argument('--phys_steps', default=2, type=int, help = 'Number of Phys Steps')
     parser.add_argument('--phys_scale', default=0.05, type=float, help = 'Number of Phys Scale')
 
     parser.add_argument('--batch_size', default=64, type=int, help = 'batch size')
-    parser.add_argument('--epochs', default=100, type=int, help = 'Number of Epochs')
+    parser.add_argument('--epochs', default=500, type=int, help = 'Number of Epochs')
     parser.add_argument('--lr', default=1e-3, type=float, help='learning rate')
-    parser.add_argument('--step_size', default=20, type=int, help='scheduler step size')
+    parser.add_argument('--wd', default=1e-4, type=float, help='weight decay')
+    parser.add_argument('--step_size', default=100, type=int, help='scheduler step size')
+    parser.add_argument('--gamma', default=0.5, type=float, help='scheduler factor')
+    parser.add_argument('--gpu', default=0, type=int, help='device number')
+
+    parser.add_argument('-tg', '--tg', default=5, type=int, help = 'time gap')
+    parser.add_argument('-Ng', '--Ng', default=1, type=int, help = 'N gap')
+    parser.add_argument('-l1', '--lambda1', default=1, type=float, help='weight of losses1')
+    parser.add_argument('-l2', '--lambda2', default=0.1, type=float, help='weight of losses2')
+    parser.add_argument('-l3', '--lambda3', default=0.1, type=float, help='weight of losses3')
+    parser.add_argument('-l4', '--lambda4', default=0.1, type=float, help='weight of losses4')
+    parser.add_argument('-fc', '--f_channels', default=1, type=int, help='channels of f encode')
     
     return parser.parse_args(argv)
 
 if __name__=='__main__':
     # args parser
-    ipt_args = get_args()
-    print(ipt_args)
+    args = get_args()
+    print(args)
 
-    # new args params
-    logs_state = 'logs/phase1_' + ipt_args.logs_fname + '_grid_pi'
-    logs_fname = 'logs/phase1_' + ipt_args.logs_fname + '_extra_grid_pi'
-    pred_log, phys_log, model_log = torch.load(logs_state)
-    args = model_log['args']
-    args.data_path = ipt_args.data_path
-    args.logs_fname = ipt_args.logs_fname + '_extra'
-
-    args.phys_gap = ipt_args.phys_gap
-    args.phys_epochs = ipt_args.phys_epochs
-    args.phys_scale = ipt_args.phys_scale
-
-    args.batch_size = ipt_args.batch_size
-    args.epochs = ipt_args.epochs
-    args.step_size = ipt_args.step_size
-    args.lr = ipt_args.lr
-
-    args.phys_scale = ipt_args.phys_scale
-    
-    # log setting
+    # logs
     logs = dict()
     logs['args'] = args
+
+    logs_fname = 'logs/phase1_' + args.logs_fname + '_grid_pi'
 
     # load data
     data_path = 'data/' + args.data_path
@@ -56,7 +54,7 @@ if __name__=='__main__':
     Ng = args.Ng
     data = LoadData(data_path)
     obs, Cd, Cl, ctr = data.split(Ng, tg)
-    logs['data_norm'] = data.normalize(method = 'logs_norm', logs = model_log)
+    logs['data_norm'] = data.normalize('unif')   # unif: min, range  norm: mean, var
     logs['pred_model'] = []
     logs['phys_model'] = []
 
@@ -78,8 +76,6 @@ if __name__=='__main__':
     nse_model = NSEModel_FNO(shape, data.dt, args)
     params_num = nse_model.count_params()
 
-    nse_model.load_state(pred_log, phys_log)
-
     print('N0: {}, nt: {}, nx: {}, ny: {}, device: {}'.format(N0, nt, nx, ny, nse_model.device))
     print(f'Cd: {logs["data_norm"]["Cd"]}')
     print(f'Cl: {logs["data_norm"]["Cl"]}')
@@ -87,7 +83,7 @@ if __name__=='__main__':
     print(f'obs: {logs["data_norm"]["obs"]}')
     print(f'param numbers of the model: {params_num}')
 
-    # extra train process
+    # train process
     for epoch in range(1, nse_model.params.epochs+1):
         nse_model.data_train(epoch, train_loader)
         if epoch % nse_model.params.phys_gap == 0 and epoch != nse_model.params.epochs:
@@ -95,12 +91,12 @@ if __name__=='__main__':
             for param in list(nse_model.phys_model.parameters()):
                 param.requires_grad = False
 
-            for phys_epoch in range(1, +1):
+            for phys_epoch in range(1, nse_model.params.phys_epochs+1):
                 nse_model.phys_train(phys_epoch, train_loader)
             
             for param in list(nse_model.phys_model.parameters()):
                 param.requires_grad = True
-        nse_model.save_log(logs)
+        # nse_model.save_log(logs)
         nse_model.test(test_loader, logs)
 
     torch.save([nse_model.pred_model.state_dict(), nse_model.phys_model.state_dict(), logs], logs_fname)
