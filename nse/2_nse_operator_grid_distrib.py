f858ec5b2196de0e9ca1797688165500cc924d62
AsamaKiseto <974616825@qq.com>
2022-10-02 07:26:57 -0700

    nse

--- nse/2_nse_operator_grid_distrib.py
+++ nse/2_nse_operator_grid_distrib.py
@@ -111,8 +111,8 @@ if __name__=='__main__':
             
             for param in list(nse_model.phys_model.parameters()):
                 param.requires_grad = True
-        if epoch % 10 == 0:
+        if epoch % 5 == 0:
             nse_model.save_log(logs)
-        nse_model.test(test_loader, logs)
+            nse_model.test(test_loader, logs)
 
     torch.save([nse_model.pred_model.state_dict(), nse_model.phys_model.state_dict(), logs], logs_fname)
