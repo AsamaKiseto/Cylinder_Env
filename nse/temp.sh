10538afbf00509d643f1bbdbffc8ab679e411e62
AsamaKiseto <974616825@qq.com>
2022-10-30 00:47:46 -0700

    nse

--- nse/temp.sh
+++ nse/temp.sh
@@ -7,16 +7,14 @@
 # python3 4_test.py
 
 python3 2_nse_operator_grid_rbc.py --phys_gap 500 --phys_epochs 0 -lf data_based
-python3 2_nse_operator_grid_rbc.py -lf baseline
+python3 2_nse_operator_grid_rbc.py -lf phys_inc
 
 python3 2_nse_operator_grid_rbc.py --phys_gap 40 --phys_epochs 5 -lf pe_5
 python3 2_nse_operator_grid_rbc.py --phys_gap 40 --phys_epochs 15 -lf pe_15
-python3 2_nse_operator_grid_rbc.py --phys_gap 40 --phys_epochs 20 -lf pe_20
 
 python3 2_nse_operator_grid_rbc.py --phys_scale 0.01 -lf ps_0.01
 python3 2_nse_operator_grid_rbc.py --phys_scale 0.05 -lf ps_0.05
 python3 2_nse_operator_grid_rbc.py --phys_scale 0.2 -lf ps_0.2
-python3 2_nse_operator_grid_rbc.py --phys_scale 0.5 -lf ps_0.5
 
 python3 2_nse_operator_grid_rbc.py --phys_random_select True --phys_scale 0.001 -lf random_select_0.001
 python3 2_nse_operator_grid_rbc.py --phys_random_select True --phys_scale 0.0001 -lf random_select_0.0001
