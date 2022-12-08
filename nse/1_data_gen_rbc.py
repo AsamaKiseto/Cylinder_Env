1b73f588915cc07d31f131ab8907e21e377eb044
AsamaKiseto <974616825@qq.com>
2022-12-07 17:26:11 -0800

    nse

--- nse/1_data_gen_rbc.py
+++ nse/1_data_gen_rbc.py
@@ -17,6 +17,7 @@ simulator = RBC(params)
 
 # N0 = 400 + 1
 N0 = 100
+N0 = 20
 nx = simulator.params['dimx']
 ny = simulator.params['dimy']
 dt = simulator.params['dt']
@@ -56,29 +57,30 @@ print(ctr.shape, obs.shape)
 
 data = [obs, temp, ctr]
 # data = [obs, temp]
-torch.save(data, 'data/nse_data_reg_rbc')
+torch.save(data, 'data/test_data/nse_data_reg_rbc_test')
+# torch.save(data, 'data/nse_data_reg_rbc')
 
 
-# evaluate
-from scripts.draw_utils import *
+# # evaluate
+# from scripts.draw_utils import *
 
-data_path = 'data/nse_data_reg_rbc'
-data = LoadDataRBC(data_path)
-obs, temp, ctr = data.get_data()
-temp = torch.cat((temp, torch.zeros(temp.shape)), dim=-1)
-# temp = torch.cat((torch.zeros(temp.shape), temp), dim=-1)
-print(obs.shape, temp.shape)
+# data_path = 'data/nse_data_reg_rbc'
+# data = LoadDataRBC(data_path)
+# obs, temp, ctr = data.get_data()
+# temp = torch.cat((temp, torch.zeros(temp.shape)), dim=-1)
+# # temp = torch.cat((torch.zeros(temp.shape), temp), dim=-1)
+# print(obs.shape, temp.shape)
 
-obs_bf = obs[:, :-1]
-obs_af = obs[:, 1:]
-error = rel_error(obs_bf.reshape(-1, 64, 64, 3), obs_af.reshape(-1, 64, 64, 3))
-print(error.mean())
+# obs_bf = obs[:, :-1]
+# obs_af = obs[:, 1:]
+# error = rel_error(obs_bf.reshape(-1, 64, 64, 3), obs_af.reshape(-1, 64, 64, 3))
+# print(error.mean())
 
-x = np.arange(64) / 64 * 2.0
-y = np.arange(64) / 64 * 2.0
-x, y = np.meshgrid(x, y)
-xl, xh  = np.min(x), np.max(x)
-yl, yh = np.min(y), np.max(y)
-xy_mesh = [x, y, xl, xh, yl, yh]
+# x = np.arange(64) / 64 * 2.0
+# y = np.arange(64) / 64 * 2.0
+# x, y = np.meshgrid(x, y)
+# xl, xh  = np.min(x), np.max(x)
+# yl, yh = np.min(y), np.max(y)
+# xy_mesh = [x, y, xl, xh, yl, yh]
 
-animate_field(obs[0, ..., :2], xy_mesh, 'state_6', 'obs', 'rbc')
+# animate_field(obs[0, ..., :2], xy_mesh, 'state_6', 'obs', 'rbc')
